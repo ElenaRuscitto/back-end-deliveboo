7afbfefb64d8640067a6f4e6a256f82e98965b8c
ElenaRuscitto <ruscitto.elena@gmail.com>
2024-06-14 15:53:39 +0200

    [add] fillable order e type

--- app/Models/order.php
+++ app/Models/order.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class order extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'code',
+        'address',
+        'email',
+        'telephone',
+        'tot',
+        'desc'
+
+    ];
 }
