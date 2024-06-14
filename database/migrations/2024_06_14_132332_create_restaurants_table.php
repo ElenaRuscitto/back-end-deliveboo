4f8eed36724fb7a8e41d7e5f9be35728f77ad7b8
Benedettopana <benedetto.pana@gmail.com>
2024-06-14 15:30:09 +0200

    [add]Restaurants Migration/Model

---
+++ database/migrations/2024_06_14_132332_create_restaurants_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->string('email')->unique();
+            $table->string('address')->unique();
+            $table->string('vat')->unique();
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('restaurants');
+    }
+};
