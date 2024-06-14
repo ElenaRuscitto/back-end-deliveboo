d813184e48b840722d85ebb15ba060a0660d02c6
enrico-amerio <amerio.enrico95@gmail.com>
2024-06-14 15:38:47 +0200

    [add] table dishes and categories

---
+++ database/migrations/2024_06_14_131447_create_dishes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('dishes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->text('desc')->nullable();
+            $table->decimal('price', 5,2);
+            $table->boolean('visibility')->default(true);
+            $table->string('image',100)->nullable();
+            $table->boolean('vegan')->nullable()->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dishes');
+    }
+};
