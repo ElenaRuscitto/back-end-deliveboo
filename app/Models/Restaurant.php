f8967e54dea3745c8779b3b9b49dce465518f7be
ElenaRuscitto <ruscitto.elena@gmail.com>
2024-06-14 18:23:23 +0200

    [fix] model type

--- app/Models/Restaurant.php
+++ app/Models/Restaurant.php
@@ -4,8 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\Types;
-
+use App\Models\Type;
 
 class Restaurant extends Model
 {
