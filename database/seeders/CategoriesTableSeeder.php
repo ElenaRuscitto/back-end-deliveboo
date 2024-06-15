bd69ad4ff0a54d2b40cf7221f6aab149f625406c
enrico-amerio <amerio.enrico95@gmail.com>
2024-06-15 11:56:35 +0200

    [add] Types and Categories seeder

---
+++ database/seeders/CategoriesTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categories = ['Antipasti','Primi', 'Secondi', 'Contorni', 'Pizze', 'Panini', 'Insalate', 'Dolci' , 'Gelati',  'Bevande', 'Birre', 'Vini' ];
+        foreach($categories as $category){
+            $new_category = new Category();
+            $new_category->name = $category;
+            $new_category->save();
+         }
+    }
+}
