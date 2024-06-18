f9c6f18a02964211cf83f0beb75d857db14fb3de
Benedettopana <benedetto.pana@gmail.com>
2024-06-18 15:04:55 +0200

    [fix]various

--- resources/views/admin/restaurants/create.blade.php
+++ resources/views/admin/restaurants/create.blade.php
@@ -117,13 +117,4 @@
         </div>
     </form>
 </div>
-
-
-
-<!--
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://cdn.jsdelivr.net/jquery.validation/1.19.3/jquery.validate.min.js"></script>
--->
-
-
 @endsection
