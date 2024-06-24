5353f4baab439e494257c6d5629bbc9e9cabecfb
Benedettopana <benedetto.pana@gmail.com>
2024-06-24 14:15:23 +0200

    [add]api & PaymentController

---
+++ app/Http/Controllers/PaymentController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+use Braintree\Gateway;
+use Illuminate\Http\Request;
+
+class PaymentController extends Controller
+{
+    public function generateToken()
+    {
+        $gateway = new Gateway([
+            'environment' => config('services.braintree.environment'),
+            'merchantId' => config('services.braintree.merchantId'),
+            'publicKey' => config('services.braintree.publicKey'),
+            'privateKey' => config('services.braintree.privateKey'),
+        ]);
+
+        $clientToken = $gateway->clientToken()->generate();
+
+        return response()->json(['token' => $clientToken]);
+    }
+
+    public function processPayment(Request $request)
+    {
+        $gateway = new Gateway([
+            'environment' => config('services.braintree.environment'),
+            'merchantId' => config('services.braintree.merchantId'),
+            'publicKey' => config('services.braintree.publicKey'),
+            'privateKey' => config('services.braintree.privateKey'),
+        ]);
+
+        $amount = $request->amount;
+        $nonce = $request->payment_method_nonce;
+
+        $result = $gateway->transaction()->sale([
+            'amount' => $amount,
+            'paymentMethodNonce' => $nonce,
+            'options' => [
+                'submitForSettlement' => true,
+            ],
+        ]);
+
+        if ($result->success) {
+            return response()->json(['success' => true, 'transaction_id' => $result->transaction->id]);
+        } else {
+            return response()->json(['success' => false, 'message' => $result->message]);
+        }
+    }
+}
