54333561c38d533f750e46cb471dacf7452b90c7
Benedettopana <benedetto.pana@gmail.com>
2024-06-28 11:01:29 +0200

    [fix]aside

--- resources/views/admin/partials/aside.blade.php
+++ resources/views/admin/partials/aside.blade.php
@@ -2,12 +2,18 @@
 
     <div class="row row-cols-1 my-5 mx-2">
         {{--TODO:DA FARE HOME --}}
-        <div class="col mb-3 my-link">
+        <div class="col mb-3 ">
 
-            <a class="nav-link" href="{{ route('admin.home')}}">Home</a>
+            <a class="nav-link d-flex justfy-content-center my-link" href="{{ route('admin.home')}}">
+                <i class="fa-solid fa-house me-md-2 pt-1"></i>
+                <span class="d-none d-md-block">Home</span>
+            </a>
         </div>
-        <div class="col mb-3 my-link">
-            <a class="nav-link" href="{{ route('admin.show')}}">Il tuo Menu</a>
+        <div class="col mb-3 ">
+            <a class="nav-link d-flex justfy-content-center my-link" href="{{ route('admin.show')}}">
+                <i class="fa-solid fa-book-open me-md-2 pt-1"></i>
+                <span class="d-none d-md-block">Il tuo Menu</span>
+            </a>
         </div>
         {{--! Debug Aggiunta ristorante --}}
         {{-- @if(Route::currentRouteName() !== 'admin.restaurants.index')
@@ -20,18 +26,27 @@
         @if (isset($restaurant) && !empty($restaurant))
 
         {{--? Aggiunta piatto --}}
-        <div class="col mb-3 my-link">
-            <a class="nav-link" href="{{ route('admin.dishes.create', $restaurant)}}">Aggiungi piatto</a>
-        </div>
-        {{--? /Aggiunta piatto --}}
-        @endif
-
-        <div class="col mb-3 my-link">
-            <a class="nav-link" href="{{ route('admin.orders.index')}}">Ordini</a>
+        <div class="col mb-3 ">
+            <a class="nav-link d-flex justfy-content-center my-link" href="{{ route('admin.dishes.create', $restaurant)}}">
+                <i class="fa-solid fa-utensils me-md-2 pt-1"></i>
+                <span class="d-none d-md-block">Aggiungi piatto</span>
+            </a>
         </div>
 
-        <div class="col mb-3 my-link">
-            <a class="nav-link" href="{{ route('admin.chart.index')}}">Statistiche</a>
+       {{--? /Aggiunta piatto --}}        @endif
+
+        <div class="col mb-3 ">
+            <a class="nav-link d-flex justfy-content-center my-link " href="{{ route('admin.orders.index')}}">
+                <i class="fa-solid fa-receipt me-md-2 pt-1"></i>
+                <span class="d-none d-md-block">Ordini</span>
+            </a>
+        </div>
+
+        <div class="col mb-3 ">
+            <a class="nav-link d-flex justfy-content-center my-link" href="{{ route('admin.chart.index')}}">
+                <i class="fa-solid fa-chart-line me-md-2 pt-1"></i>
+                <span class="d-none d-md-block">Statistiche</span>
+            </a>
         </div>
 
     </div>
