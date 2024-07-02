a8b17610766f97facb38622e87e8a5a8305ca41e
Benedettopana <benedetto.pana@gmail.com>
2024-07-02 12:10:15 +0200

    [fix]aside

--- resources/views/admin/partials/aside.blade.php
+++ resources/views/admin/partials/aside.blade.php
@@ -6,13 +6,13 @@
 
             <a class="nav-link d-flex justfy-content-center my-link" href="{{ route('admin.home')}}">
                 <i class="fa-solid fa-house me-md-2 pt-1"></i>
-                <span class="d-none d-md-block">Home</span>
+                <span class="d-none d-xl-block">Home</span>
             </a>
         </div>
         <div class="col mb-3 ">
             <a class="nav-link d-flex justfy-content-center my-link" href="{{ route('admin.show')}}">
                 <i class="fa-solid fa-book-open me-md-2 pt-1"></i>
-                <span class="d-none d-md-block">Il tuo Menu</span>
+                <span class="d-none d-xl-block">Il tuo Menu</span>
             </a>
         </div>
         {{--! Debug Aggiunta ristorante --}}
@@ -38,14 +38,14 @@
         <div class="col mb-3 ">
             <a class="nav-link d-flex justfy-content-center my-link " href="{{ route('admin.orders.index')}}">
                 <i class="fa-solid fa-receipt me-md-2 pt-1"></i>
-                <span class="d-none d-md-block">Ordini</span>
+                <span class="d-none d-xl-block">Ordini</span>
             </a>
         </div>
 
         <div class="col mb-3 ">
             <a class="nav-link d-flex justfy-content-center my-link" href="{{ route('admin.chart.index')}}">
                 <i class="fa-solid fa-chart-line me-md-2 pt-1"></i>
-                <span class="d-none d-md-block">Statistiche</span>
+                <span class="d-none d-xl-block">Statistiche</span>
             </a>
         </div>
 
